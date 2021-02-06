3e4484f1862be64e0ae888471713cc1555314844
SamratKBTU <78403265+ProximaSamrat@users.noreply.github.com>
2021-02-06 15:19:47 +0600

    Update b2.cpp

--- Quiz 3_2/b2.cpp
+++ Quiz 3_2/b2.cpp
@@ -15,12 +15,6 @@ int main ()
             cin >> arr[i];
         }
         
-      /*  for ( int i = 0; i < 10; i++ ) {                                    //n - ???????????
-        for ( int j = i+1; j < 10; j++ ) { 
-            if ( arr[i] == arr[j] ) {
-                         cout << "|" << j << " ? " << i << "|"  << endl;
-            }
-        }*/
    int repeat[n] = {0};
    for (int i = 0; i < n; i++) repeat[arr[i]] +=1;
    for (int i = 0; i < n; i++)  
